5c31aca232f686440068467a50493bbf2accad7d
Eric Periou <eperiou@yahoo.com>
2017-04-26 12:56:08 -0500

    (update) update webpack to webpack2, add loaders for jsx,js,css,sass

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,51 @@ const DEV = path.resolve(__dirname, 'Client/Components');
 const OUTPUT = path.resolve(__dirname, 'Client/Output');
 
 const config = {
+  context: path.join(__dirname, './Client'),
   entry: `${DEV}/index.jsx`,
   output: {
     path: OUTPUT,
     filename: 'myCode.js',
   },
   module: {
-    loaders: [{
-      include: DEV,
-      loader: 'babel-loader',
-    }],
+    // loaders: [{
+    //   include: DEV,
+    //   loader: 'babel-loader',
+    // }],
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Check for all js files
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] },
+        }],
+      },
+      {
+        test: /\.(sass|scss)$/, // Check for sass or scss file names
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    //   {
+    //     test: /\.json$/,
+    //     loader: 'json-loader',  //JSON loader
+    //   },
+    ],
   },
+  devServer: {
+    contentBase: DEV,
+  },
+  devtool: 'eval-source-map',
 };
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = ''; // No sourcemap for production
+
+  // Add more configuration for production here like
+  // Uglify plugin
+  // Offline plugin
+  // Etc,
+}
 
 module.exports = config;
