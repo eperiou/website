c01059a6cc7e45a8485705a26bfb600134144eee
Eric Periou <eperiou@yahoo.com>
2017-05-01 15:34:19 -0500

    (feature) Add Selection and style page

---
+++ Client/Components/Splashpage/selections.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const Selections = () => (
+  <article className="row presentationblocks">
+    <div className="presentationtitle">
+      <p>Me in a nutshellasdfasdfasdf asdfs dafdsa df</p>
+    </div>
+    <div className="col-md-3 presentblock">
+      <div className="thumbnail">
+        <img src="..." alt="..." />
+        <div className="caption">
+          <h3>Sample Projects</h3>
+          <p> A sampling of some of my personal projects.
+            They have been either solo or in collaboration
+          </p>
+          <p>
+            <a
+              href="#"
+              className="btn btn-primary"
+              role="button"
+            >Button
+           </a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="col-md-3 presentblock">
+      <div className="thumbnail">
+        <img src="..." alt="..." />
+        <div className="caption">
+          <h3>Thumbnail label</h3>
+          <p>...</p>
+          <p>
+            <a
+              href="#"
+              className="btn btn-primary"
+              role="button"
+            >Button</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="col-md-3 presentblock">
+      <div className="thumbnail">
+        <img src="..." alt="..." />
+        <div className="caption">
+          <h3>Thumbnail label</h3>
+          <p>...</p>
+          <p>
+            <a
+              href="#"
+              className="btn btn-primary"
+              role="button"
+            >Button</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </article>
+);
+
+export default Selections;
